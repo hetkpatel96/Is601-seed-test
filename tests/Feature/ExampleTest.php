debe4c43eaaf2f755d618ea0c4663fc8142a5fb3
Het patel <hp428@njit.edu>
2018-10-24 17:54:55 -0400

    Unit test 6: test for request response 404

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -19,4 +19,10 @@ class ExampleTest extends TestCase
         $response->assertStatus(200);
 
     }
+
+    public function testStatus()
+    {
+        $response = $this->get('/njit/bag');
+        $response->assertStatus(404);
+    }
 }
